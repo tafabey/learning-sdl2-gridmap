74698d232d0f8854680302f7a1b6280ecc1d8084
tafabey <muhammedmustafademir@proton.me>
2026-01-03 08:12:06 +0000

    Add LAVA tile with solid and damage behavior

--- learning-sdl2-gridmap.c
+++ learning-sdl2-gridmap.c
@@ -1,13 +1,33 @@
 #include <SDL2/SDL.h>
 
-#define COLS 10
 #define ROWS 10
+#define COLS 10
+
+typedef enum {
+    NONE = 0, // 0
+    SOLID = 1 << 0, // 0x01
+    DAMAGE = 1 << 1 // 0x02
+} TileFlag;
+
+typedef enum {
+    EMPTY = 0,
+    WALL = EMPTY | SOLID,
+    THORN = EMPTY | DAMAGE,
+    LAVA = EMPTY | SOLID + DAMAGE
+} Block;
+
+int block_to_flags(Block block) {
+    return (int) block;
+}
 
 int main() {
     int nx = 0, ny = 0;
-    int arr[COLS][ROWS];
+    Block arr[ROWS][COLS];
     FILE *fp = fopen("tile.txt", "r");
-    
+    if (fp == NULL) {
+	perror("Failed to open file: ");
+	return -1;
+    }
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *window = SDL_CreateWindow("SDL Texture denemesi", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
 
@@ -18,12 +38,17 @@ int main() {
 
     SDL_Surface *spriteSurface = SDL_CreateRGBSurfaceWithFormat(0, 32, 32, 32, SDL_PIXELFORMAT_RGBA32);
     SDL_FillRect(spriteSurface, NULL, SDL_MapRGB(spriteSurface->format, 139, 69, 19));
-    
+    int health = 100;
+
+    SDL_Surface *lavaSurface = SDL_CreateRGBSurfaceWithFormat(0, 32, 32, 32, SDL_PIXELFORMAT_RGBA32);
+    SDL_FillRect(lavaSurface, NULL, SDL_MapRGB(lavaSurface->format, 255, 117, 0));
     SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_Texture *sprite = SDL_CreateTextureFromSurface(renderer, spriteSurface);
+    SDL_Texture *lavaTexture = SDL_CreateTextureFromSurface(renderer, lavaSurface);
 
     SDL_FreeSurface(surface);
     SDL_FreeSurface(spriteSurface);
+    SDL_FreeSurface(lavaSurface);
     
     SDL_Rect srcRect = {0, 0, 32, 32};
     SDL_Rect dstRect = {0, 0, 32, 32};
@@ -33,19 +58,24 @@ int main() {
     SDL_RenderCopy(renderer, texture, &srcRect, &dstRect);
     SDL_RenderPresent(renderer);
 
-    SDL_Event event;
-    int running = 1;
-    
+    int temp_block;
     for (int i = 0; i < COLS; i++) {
 	for (int j = 0; j < ROWS; j++) {
-	    if (arr[i][j] == 1) {
-		dstRect.x = j * 32;
-		dstRect.y = i * 32;
+	    fscanf(fp, "%d", &temp_block);
+	    arr[i][j] = (Block) temp_block;
+	    dstRect.x = j * 32;
+	    dstRect.y = i * 32;
+	    if (arr[i][j] == WALL)
 		SDL_RenderCopy(renderer, texture, &srcRect, &dstRect);
-	    }
+	    else if (arr[i][j] == LAVA)
+		SDL_RenderCopy(renderer, lavaTexture, &srcRect, &dstRect);
 	}
     }
 
+    SDL_Event event;
+    int running = 1;
+    int block_flags;
+   
     while (running) {
 	if (SDL_PollEvent(&event)) {
 	    if (event.type == SDL_QUIT) {
@@ -53,33 +83,45 @@ int main() {
 	    } else if (event.type == SDL_KEYDOWN) {
 		switch(event.key.keysym.sym) {
 		case SDLK_RIGHT: {
-		    if (arr[ny][nx + 1] != 0) {
+		    block_flags = block_to_flags(arr[ny][nx + 1]);
+		    if (!(block_flags & SOLID)) {
 			spriteRect.x += 32;
 			nx++;
 		    }
 		    break;
 		}
 		case SDLK_LEFT: {
-		    if (arr[ny][nx - 1] != 0) {
+		    block_flags = block_to_flags(arr[ny][nx - 1]);
+		    if (!(block_flags & SOLID)) {
 			spriteRect.x -= 32;
 			nx--;
 		    }
 		    break;
 		}
 		case SDLK_UP: {
-		    if (arr[ny - 1][nx] != 0) {
+		    block_flags = block_to_flags(arr[ny - 1][nx]);
+		    if (!(block_flags & SOLID)) {
 			spriteRect.y -= 32;
 			ny--;
 		    }
 		    break;
 		}
 		case SDLK_DOWN: {
-		    if (arr[ny + 1][nx] != 0) {
+		    block_flags = block_to_flags(arr[ny + 1][nx]);
+		    if (!(block_flags & SOLID)) {
 			spriteRect.y += 32;
 			ny++;
 		    }
 		    break;
 		}
+		}
+		if (block_flags & DAMAGE) {
+		    health -= 5;
+		    printf("Health decreased by 5\n");
+		}
+		if (health <= 0) {
+		    printf("Game over, your health is 0\n");
+		    break;
 		}
 	    }
 	}
@@ -87,11 +129,12 @@ int main() {
 	for (int i = 0; i < COLS; i++) {
 	    for (int j = 0; j < ROWS; j++) {
 		fscanf(fp, "%d", &arr[i][j]);
-		if (arr[i][j] == 1) {
-		    dstRect.x = j * 32;
-		    dstRect.y = i * 32;
+		dstRect.x = j * 32;
+		dstRect.y = i * 32;
+		if (arr[i][j] == WALL)
 		    SDL_RenderCopy(renderer, texture, &srcRect, &dstRect);
-		}
+		else if (arr[i][j] == LAVA)
+		    SDL_RenderCopy(renderer, lavaTexture, &srcRect, &dstRect);
 	    }
 	}
 
